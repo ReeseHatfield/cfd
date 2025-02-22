5286adfc5e92012ca5279fe70b2d4b6899ed7b9e
ReeseHatfield <hatfield.69@wright.edu>
2025-02-22 11:12:04 -0500

    begin editor config
    still need nvim and vim run impl

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # users gotta have cargo 
-if ! command -v cargo &> /dev/null; then
-    echo "Cargo is a prereq. and must be installed to isntall cfd"
-    exit 1
-fi
+
+# export PATH="$HOME/.local/bin:$PATH"
 
 # steal from run.sh
 RUSTFLAGS="-D warnings" cargo build --release --target-dir bin
@@ -15,9 +13,20 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-INSTALL_DIR="/usr/local/bin"
+INSTALL_DIR="$HOME/.local/bin"
 EXECUTABLE_NAME="cfd"  
 
 cp bin/release/$EXECUTABLE_NAME "$INSTALL_DIR"
+
+CONFIG_DIR="${HOME}/.config/cfd"
+
+mkdir -p "${CONFIG_DIR}"
+
+echo "creating config"
+cat > "${CONFIG_DIR}/cfd.json"<< EOF
+{
+  "editor": "Neovim"
+}
+EOF
 
 echo "Installed $EXECUTABLE_NAME to $INSTALL_DIR."
--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,143 @@ use std::process::Command;
 use std::{env, io};
 
 use crossterm::event::{KeyCode, KeyEvent};
-use crossterm::{event, ExecutableCommand};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use ratatui::prelude::CrosstermBackend;
-use ratatui::Terminal;
-use ratatui::crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{event, ExecutableCommand};
 use nix::sys::signal::{kill, Signal};
 use nix::unistd::Pid;
+use ratatui::crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use ratatui::prelude::CrosstermBackend;
+use ratatui::Terminal;
 
 use ratatui::{prelude::*, widgets::*};
+use shellexpand::tilde;
+use strum_macros::{Display, EnumString};
+
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::fs;
 
 
-fn main() -> Result<(), Box<dyn Error>>{
+struct VSCode();
 
-    let options = get_options()?;
+struct Neovim();
 
-    if options.len() == 1 {
-        run_c(options[0].clone())?;
-        kill_parent_procss();
+struct Vim();
 
+
+pub trait Editor {
+    fn run(&self, path: &str) -> Result<(), Box<dyn Error>>;
+    fn get_editor_cmd(&self) -> &'static str;
+} 
+
+
+impl Editor for Neovim {
+
+    fn run(&self, path: &str) -> Result<(), Box<dyn Error>> {
         return Ok(())
     }
 
-    enable_raw_mode()?;
+    fn get_editor_cmd(&self) -> &'static str {
+        "nvim"
+    }
 
+}
+
+impl Editor for Vim {
+
+    fn run(&self, path: &str) -> Result<(), Box<dyn Error>> {
+        return Ok(())
+    }
+
+    fn get_editor_cmd(&self) -> &'static str {
+        "vim"
+    }
+
+}
+
+impl Editor for VSCode {
+    fn run(&self, path: &str) -> Result<(), Box<dyn Error>> {
+
+        let _child = Command::new(self.get_editor_cmd())
+            .arg("-n")
+            .arg(path)
+            .output()?;
+
+        kill_parent_procss();
+
+        return Ok(());
+    }
+
+    fn get_editor_cmd(&self) -> &'static str {
+        "code"
+    }
+
+}
+
+
+struct SupportedEditor(Box<dyn Editor>);
+
+
+
+
+struct Config {
+    editor: SupportedEditor
+}
+
+
+
+fn get_config() -> Result<Config, Box<dyn Error>> {
+
+
+    let expand_me = tilde("~/.config/cfd/cfd.json");
+    let config_file_resolved = expand_me.as_ref();
+
+    let content = fs::read_to_string(config_file_resolved)?;
+
+    /*
+    {
+        "editor": "Neovim"
+    }
+     */
+
+     // horrible but I dont wanna use serde because the structs dont match ;-;
+    let editor_name = content
+        .replace("{", "")
+        .replace("}", "")
+        .split(":")
+        .map(|s| s.to_owned())
+        .collect::<Vec<String>>()[1]
+        .replace(" ", "")
+        .replace("\"","");
+
+
+    let editor: SupportedEditor = match editor_name.as_str() {
+        "Neovim" => { SupportedEditor(Box::new(Neovim {})) },
+        "Vim" => { SupportedEditor(Box::new(Vim{}))},
+        "VSCode" => { SupportedEditor(Box::new(VSCode{}))}
+        _ => return Err("Unsupported editor".into())
+    };
+
+    return Ok(Config{
+        editor: editor
+    });
+
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    let config = get_config()?;
+
+    // println!("config was {:?}", config);
+    let options = get_options()?;
+
+    if options.len() == 1 {
+        config.editor.0.run(&options[0])?;
+
+        return Ok(());
+    }
+
+    enable_raw_mode()?;
 
     let mut stdout = io::stdout();
     stdout.execute(EnterAlternateScreen)?;
@@ -38,18 +151,16 @@ fn main() -> Result<(), Box<dyn Error>>{
 
     let mut selected_index = 0;
 
-
     loop {
-
         terminal.draw(|frame| {
             let size = frame.area();
             let width = size.width;
             let height = size.height;
-        
-            let start_y = 1; 
-        
+
+            let start_y = 1;
+
             let menu_area = Rect::new(0, start_y, width, height - start_y);
-        
+
             let menu: Vec<ListItem> = options
                 .iter()
                 .enumerate()
@@ -62,13 +173,12 @@ fn main() -> Result<(), Box<dyn Error>>{
                     ListItem::new(item.as_str()).style(style)
                 })
                 .collect();
-        
-            let list = List::new(menu)
-                .block(Block::default().borders(Borders::ALL).title("cfd"));
-        
+
+            let list = List::new(menu).block(Block::default().borders(Borders::ALL).title("cfd"));
+
             frame.render_widget(list, menu_area);
         })?;
-        
+
         // you'd think a tui app would just have an api for this
         if let event::Event::Key(KeyEvent { code, .. }) = event::read()? {
             match code {
@@ -87,7 +197,7 @@ fn main() -> Result<(), Box<dyn Error>>{
                     //graceful exit?
                     std::process::exit(1);
                     // also need this for ctrl+c
-                },
+                }
                 _ => {}
             }
         }
@@ -95,41 +205,31 @@ fn main() -> Result<(), Box<dyn Error>>{
 
     disable_raw_mode()?;
 
-    let selected_dir = options[selected_index].clone();
+    // let selected_dir = options[selected_index].clone();
 
     terminal.backend_mut().execute(LeaveAlternateScreen)?;
-    
-    run_c(selected_dir)?;
 
-    // kill parent process 
+
+
+
+    // run_editor(selected_dir, config.editor)?;
+
+    // kill parent process
     // turn shell into code instance
 
-    kill_parent_procss();
+    // kill_parent_procss();
     Ok(())
 }
 
-
-fn kill_parent_procss(){
+fn kill_parent_procss() {
     let ppid = unsafe { libc::getppid() };
     if ppid > 1 {
         let _ = kill(Pid::from_raw(ppid), Signal::SIGKILL);
     }
-
-}
-
-fn run_c(selected_dir: String) -> Result<(), Box<dyn Error>>{
-
-    let _child = Command::new("code")
-        .arg("-n")
-        .arg(selected_dir)
-        .output();
-
-    Ok(())
 }
 
 
 fn parse_args() -> Result<String, Box<dyn Error>> {
-
     let full_args = env::args();
 
     if full_args.len() == 1 {
@@ -145,7 +245,6 @@ fn parse_args() -> Result<String, Box<dyn Error>> {
     Ok(query)
 }
 
-
 fn get_options() -> Result<Vec<String>, Box<dyn Error>> {
     let z_query = parse_args()?;
 
@@ -154,7 +253,7 @@ fn get_options() -> Result<Vec<String>, Box<dyn Error>> {
         .arg("-l")
         .arg(z_query)
         .output()?;
-    
+
     let query_res = str::from_utf8(&child.stdout)?;
 
     let options: Vec<String> = query_res
@@ -164,5 +263,4 @@ fn get_options() -> Result<Vec<String>, Box<dyn Error>> {
         .collect();
 
     Ok(options)
-
 }
