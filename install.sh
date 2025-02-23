3bc4c763459a26032388a63a28ed548b95ec3fd0
ReeseHatfield <hatfield.69@wright.edu>
2025-02-23 18:33:21 -0500

    better working install

--- install.sh
+++ install.sh
@@ -13,7 +13,15 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# remove any old installations
+
 INSTALL_DIR="$HOME/.local/bin"
+
+# ./home/reese/.local/bin/cfd
+# ./usr/local/bin/cfd
+
+# rm /usr/local/bin/cfd
+rm "$INSTALL_DIR/cfd"
 EXECUTABLE_NAME="cfd"  
 
 cp bin/release/$EXECUTABLE_NAME "$INSTALL_DIR"
